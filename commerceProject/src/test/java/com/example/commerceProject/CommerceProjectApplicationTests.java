301add5af78ec24599ecda20cdab480b78b71f7d
tlh07930 <89278658+tlh07930@users.noreply.github.com>
2021-10-25 23:01:51 -0500

    Add files via upload

---
+++ commerceProject/src/test/java/com/example/commerceProject/CommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commerceProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
