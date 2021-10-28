67c113bd85248837d41f6bb02ac9b5e2192e09d1
bma63940 <bma63940@ucmo.edu>
2021-10-28 14:02:27 -0500

    Adding bootstrap support for button and POST method

--- commerceProject/src/js/src/calendar/calendar.js
+++ commerceProject/src/js/src/calendar/calendar.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import CalendarPicker from './calendar-picker';
 import './calendar.css';
+import Button from 'react-bootstrap/Button';
 
 const Calendar = () => {
     const [startDay, setStartDay] = useState('');
     const [endDay, setEndDay] = useState('');
-    const KEYMAP_TO_DB = "localhost:8080/calendar/submit"
+    const KEYMAP_TO_DB = "http://localhost:8080/calendar/submit"
 
     const handle_startDay= (day) => {
         const newDate = new Date(day.getFullYear(), day.getMonth(), day.getDate());
@@ -19,19 +20,51 @@ const Calendar = () => {
         }
         else setEndDay('');
     }
+    
+    const submitClick = () => {
 
-    const onSubmit = (e) => {
+    }
+
+    const onSubmit = async (e) => {
         e.preventDefault();
         console.log(e);
+        const check1 = validate_day(startDay);
+        const check2 = validate_day(endDay);
+
+        const validated = check1 && check2 ? true : false;
+        try {
+        validated ? (
+                fetch(KEYMAP_TO_DB, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({startDay: startDay, endDay: endDay})
+            })
+            .then(console.log(JSON.stringify({startDay: startDay, endDay: endDay})))
+            .then()
+        ) : alert("Select two days before submitting.");
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+    function validate_day (day) {
         
+        return day ? true : false;
     }
     return(
     <>
         <CalendarPicker startDay={startDay} endDay={endDay} handle_startDay={handle_startDay} handle_endDay={handle_endDay}/>
-        <form id="submit-reservation" action={KEYMAP_TO_DB} method="post" onSubmit={onSubmit}>
-            <input id="form-start" required="required" type="text" name="startDay" value={startDay} onChange={handle_startDay}/>
-            <input id="form-end" required="required" type="text" name="endDay" value={endDay} onChange={handle_endDay}/>
-            <button id="submit" type="submit" value="Submit"/>
+        <form>
+        <div>
+            <div className="form-group">
+            <Button variant="outline-success" id="submit-reservation" type="submit" 
+            onClick={onSubmit} 
+            disabled={!startDay || !endDay }>Submit</Button>{''}
+            </div>
+        </div>
         </form>
     </>
     )
